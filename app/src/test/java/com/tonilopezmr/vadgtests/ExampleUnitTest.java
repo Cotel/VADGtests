59c197d031b582417c5376a8ab4906dd402acf21
Antonio López Marín <tonilopezmr@gmail.com>
2017-09-20 18:17:27 +0200

    Added hamcrest assert

--- app/src/test/java/com/tonilopezmr/vadgtests/ExampleUnitTest.java
+++ app/src/test/java/com/tonilopezmr/vadgtests/ExampleUnitTest.java
@@ -2,16 +2,14 @@ package com.tonilopezmr.vadgtests;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
 public class ExampleUnitTest {
   @Test
   public void addition_isCorrect() throws Exception {
     assertEquals(4, 2 + 2);
+    assertEquals(4, is(2 + 2));
   }
 }
